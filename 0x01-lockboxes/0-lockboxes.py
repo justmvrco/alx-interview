993275f46d7a5283f619fbe0530cff96dd6b3f5e
justmvrco <moreirapiliki@gmail.com>
2022-04-14 20:17:34 +0000

    alx-interview task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,36 +1,52 @@
 #!/usr/bin/python3
-"""
-Module 0-lockboxes
-"""
+
+'''
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1 and
+each box may contain keys to the other boxes.
+Write a method that determines if all the boxes can be opened.
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+    There can be keys that do not have boxes
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+'''
+
+
+def join(list_1, list_2):
+    '''return a list of elements '''
+    result = []
+    for elem in list_2:
+        result += list_1[elem]
+    return result
 
 
 def canUnlockAll(boxes):
-    """
-    You have n number of locked boxes in front of you.
-    Each box is numbered sequentially from 0 to n - 1,
-    and each box may contain keys to the other boxes.
-    Determine if all the boxes can be opened.
-    """
-    if boxes is None:
-        return False
-    if len(boxes) == 1:
-        return True
-    # Track the visited boxes
-    visited = set()
-    # The first box is unlocked
-    visited.add(0)
+    ''' return T if all boxes can be opened else, F '''
+    boxes = boxes if boxes and isinstance(boxes, list) else []
+    idx = 0
+    total = list(set(boxes[0]) | {0})
+    can_open = True
+    while can_open:
+        can_open = False
+        for i in join(boxes, total[idx:]):
+            if i not in total:
+                total.append(i)
+                idx += 1
+                can_open = True
+    return len(total) == len(boxes)
 
-    # Use a stack to do a Depth First Search
-    stack = []
-    stack.append(0)
 
-    while stack:
-        keys = boxes[stack.pop()]
-        for key in keys:
-            if key not in visited:
-                visited.add(key)
-                stack.append(key)
-    # All have been visited if the
-    # number of boxes is equal to the
-    # number of visited boxes
-    return len(boxes) == len(visited)
+if __name__ == '__main__':
+    canUnlockAll = __import__('0-lockboxes').canUnlockAll
+
+    boxes = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes))
+
+    boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes))
